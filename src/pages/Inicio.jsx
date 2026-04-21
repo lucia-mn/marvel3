ac7d7dcefeddf8fd7c4fbe0cdcabaa39704b17ce
Lucia Mendiola Naharro <lmendiol@emeal.nttdata.com>
2026-04-21 16:56:15 +0200

    fecth

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 import React from 'react';
 import { TarjetaHeroe } from "../components/TarjetaHeroe";
+import {obtenerComics} from "../services/comicVineApi";
 
 
 export const Inicio = ({nextPage, more}) => {
+
+    console.log(obtenerComics);
 
     return (
         <>
