e2ff946216ae64ecfc8432d1b18ad96239852077
Lucia Mendiola Naharro <lmendiol@emeal.nttdata.com>
2026-04-21 16:05:31 +0200

    inicio-tarjetas

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import React from 'react';
+import { TarjetaHeroe } from "../components/TarjetaHeroe";
 
-export const Inicio = () => {
-  return (
-    <div>Inicio</div>
-  )
+
+export const Inicio = ({nextPage, more}) => {
+
+    return (
+        <>
+            <TarjetaHeroe />
+
+            {/* {more &&
+                <button className='boton' onClick={nextPage}>
+                    Ver más personajes
+                </button>
+            } */}
+        </>
+
+    );
 }
